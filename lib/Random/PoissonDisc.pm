6cf03321c8a94d5554c0e408ee64ab85dd62455e
Max Maischein <corion@corion.net>
2011-06-04 13:48:47 +0200

    Reswitch algorithm to one alluded to by DE Knuth

--- lib/Random/PoissonDisc.pm
+++ lib/Random/PoissonDisc.pm
@@ -14,12 +14,44 @@ Random::PoissonDisc - distribute points aesthetically in a plane
 
 sub rnd {
     my ($low,$high) = @_;
-    return $low + random($high-$low);
+    return $low + rand($high-$low);
 };
+
+use Data::Dumper;
 
 sub grid_coords {
     my ($size,$point) = @_;
-    join "\t", map { int($_/$size) } @$_;
+    join "\t", map { int($_/$size) } @$point;
+};
+
+# Return x(i)+e(i) and x(i)-e(i)
+# by using the grid
+sub neighbour_points {
+    my ($size,$point) = @_;
+    my @coords = split /\t/, grid_coords( $size, $point );
+    map {}
+    map {} @coords;
+};
+
+sub random_unit_vector {
+    # idea taken from http://burtleburtle.net/bob/rand/unitvec.html
+    # resp. Knuth, _The Art of Computer Programming_, vol. 2,
+    # 3rd. ed., section 3.4.1.E.6.
+    # but not verified
+    my ($dimensions) = @_;
+    my (@vec,$len);
+    
+    # Create normal distributed coordinates
+    # around 0 in (-1,1)    
+    RETRY: {
+        @vec = map { 1-2*gaussian() } 1..$dimensions;
+        # Normalize our vector:
+        $len = sqrt( sum @{[map {$_**2} @vec]} );
+        redo RETRY unless $len;
+    };
+    @vec = map { $_ / $len } @vec;
+    
+    \@vec
 };
 
 sub points {
@@ -31,10 +63,13 @@ sub points {
     $options{r} ||= 10;
     #$options{max} ||= 10; # we want to fill the space instead?!
     
+    # See http://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
     # This should adapt to multiple dimensions
-    # I guess it's radius / n-th root of 2.
+    # I guess it's radius / n-th root of n.
     # XXX Need to think about this
-    my $grid_size = $options{ r } / sqrt(2);
+    my $grid_size = $options{ r } / sqrt( 0+@{$options{dimensions}});
+    # A grid can't nicely model discs, so this is only an approximation
+    # Pfeh.
     
     my @result;
     my @work;
@@ -49,30 +84,44 @@ sub points {
     $grid{ $c } = $p;
     
     while (@work) {
-        my $p = splice @work, rnd(0,$#work,1);
-        for (1..$options{ candidates }) {
+        my $origin = splice @work, int rnd(0,$#work), 1;
+        CANDIDATE: for my $candidate ( 1..$options{ candidates } ) {
             # Create a random distance between r and 2r
             # that is, in the annulus with radius (r,2r)
             # surrounding our current point
-            my $dist = rnd( $options{r}, $options{ r}*2 );
+            my $dist = rnd( $options{r}, $options{r}*2 );
             
-            # Now choose a random angle in which to point
+            # Choose a random angle in which to point
             # this vector
-            my $angle = [map{...}] 0..$#{$options{dimensions}};
-            #
+            my $angle = random_unit_vector(0+@{$options{ dimensions}});
             
-            # check grid
+            # Generate a new point by adding the $angle*$dist to $origin
+            my $p = [map { $origin->[$_] + $angle->[$_]* $dist } 0..$#$angle];
+            
+            # Check whether our point lies within the dimensions
+            for (0..$#$p) {
+                do { #warn "$candidate Rejecting";
+                     ; next CANDIDATE }
+                    if   $p->[$_] >= $options{ dimensions }->[ $_ ]
+                      or $p->[$_] < 0
+            };
+            
+            # check discs by using the grid
+            # Here we should check the "neighbours" in the grid too
             my $c = grid_coords($grid_size, $p);
             if (! $grid{ $c }) {
                 # if not already in grid, add it
                 push @result, $p;
                 push @work, $p;
                 $grid{ $c } = $p;
+                #warn "$candidate Taking";
+            } else {
+                #warn "$candidate Occupied";
             };
         };
     };
     
-    @result
+    \@result
 };
 
 1;
