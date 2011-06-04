f7653d7f91f4d31d876f770a05307ce59df96c70
Max Maischein <corion@corion.net>
2011-06-04 14:14:06 +0200

    Make the radius a command line option

--- bin/random-poissondisc.pl
+++ bin/random-poissondisc.pl
@@ -1,10 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 use Random::PoissonDisc;
+use Getopt::Long;
+
+GetOptions(
+    'r:s' => \my $r,
+);
+
+$r ||= 10;
 
 my $points = Random::PoissonDisc->points(
     dimensions => [100,100],
-    r => 10,
+    r => $r,
 );
 
 for (@$points) {
